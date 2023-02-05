af43fe32dcfed2d102ffb87ca4eeaf4350bc1723
Mokando <lex.gr@yandex.ru>
2023-02-05 20:23:45 +0300

    + Deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+export DEBIAN_FRONTEND=noninteractive && \
+apt update && apt upgrade --yes && \
+apt install --yes python3-pip git && \
+pip3 install ansible && \
+cd /tmp && \
+git clone https://github.com/mokando/ansible_sui_node.git && \
+cd /tmp/ansible_sui_node && \
+ansible-playbook ./playbooks/main.yml -i "127.0.0.1," -c local
